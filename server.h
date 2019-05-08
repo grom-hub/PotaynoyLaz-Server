56fcdd8df18f705d5740e54aab11f60e7416aa46
tixiydrakon <ya4ule@gmail.com>
2019-05-08 22:08:38 +0300

    start

---
+++ server.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <set>
+
+#include "game.h"
+
+const int SEND_BUFF_SIZE = 102400;
+const int RECV_BUFF_SIZE = 1024;
+
+class Server
+{
+public:
+    void initServer();
+    void closeConnection();
+    void mainLoop(Game &gm);
+
+
+
+
+private:
+    int listener;
+    struct sockaddr_in addr;
+    std::set<int> clients;
+
+    char recvBuff[RECV_BUFF_SIZE];
+    char sendPreBuff[SEND_BUFF_SIZE];
+    char sendBuff[SEND_BUFF_SIZE];
+
+    int bytes_read;
+
+    int bytesSend;
+    int totalBytesSend;
+    int targetSendSize;
+    int sendSize;
+
+    int clientid;
+
+
+};
